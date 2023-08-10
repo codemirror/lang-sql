ecee754b2843dbe8426e7616dce7fe207427b1da
Marijn Haverbeke <marijn@haverbeke.berlin>
2023-08-10 15:57:09 +0200

    Remove negative lookbehind
    FIX: Remove use of negative lookbehind in a regular expression, which recent
    versions of Safari still don't support.
    
    Closes https://github.com/codemirror/dev/issues/1227

--- src/complete.ts
+++ src/complete.ts
@@ -127,7 +127,8 @@ export function completeFromSchema(schema: {[table: string]: readonly (string | 
   let idQuote = dialect?.spec.identifierQuotes?.[0] || '"'
   let defaultSchema = top.child(defaultSchemaName || "", idQuote)
   for (let table in schema) {
-    let parts = table.split(/(?<!\\)\./), base = parts.length == 1 ? defaultSchema : top
+    let parts = table.replace(/\\?\./g, p => p == "." ? "\0" : p).split("\0")
+    let base = parts.length == 1 ? defaultSchema : top
     for (let part of parts) base = base.child(part.replace(/\\\./g, "."), idQuote)
     for (let option of schema[table]) if (option)
       base.list.push(typeof option == "string" ? nameCompletion(option, "property", idQuote) : option)
