942d52cd46f64cf75bfb534609cde4f4e33aec8b
Denys Zinoviev Valerijovich <denys.zinoviev@datuum.ai>
2025-07-09 18:07:06 +0200

    Update MSSQL with the missing temporal types

--- src/sql.ts
+++ src/sql.ts
@@ -251,7 +251,7 @@ export const MariaSQL = SQLDialect.define({
 /// Server](https://www.microsoft.com/en-us/sql-server).
 export const MSSQL = SQLDialect.define({
   keywords: SQLKeywords + "trigger proc view index for add constraint key primary foreign collate clustered nonclustered declare exec go if use index holdlock nolock nowait paglock pivot readcommitted readcommittedlock readpast readuncommitted repeatableread rowlock serializable snapshot tablock tablockx unpivot updlock with",
-  types: SQLTypes + "bigint smallint smallmoney tinyint money real text nvarchar ntext varbinary image hierarchyid uniqueidentifier sql_variant xml",
+  types: SQLTypes + "smalldatetime datetimeoffset datetime2 datetime bigint smallint smallmoney tinyint money real text nvarchar ntext varbinary image hierarchyid uniqueidentifier sql_variant xml",
   builtin: "binary_checksum checksum connectionproperty context_info current_request_id error_line error_message error_number error_procedure error_severity error_state formatmessage get_filestream_transaction_context getansinull host_id host_name isnull isnumeric min_active_rowversion newid newsequentialid rowcount_big xact_state object_id",
   operatorChars: "*+-%<>!=^&|/",
   specialVar: "@"
